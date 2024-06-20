e4f984cf8f94655bb5e3e2d7362774dfc241c360
yedid <yedidyakfir@gmail.com>
2024-06-20 22:20:26 +0300

    fix tests for maching patterns

--- tests/utils/test__regex.py
+++ tests/utils/test__regex.py
@@ -7,15 +7,19 @@ from runner.utils.regex import get_values_from_matching_patterns
 
 @pytest.mark.parametrize(
     ["text", "expected"],
-    [["classA", "Any"], ["CLASss.classB.asdfas", "Dict"], ["classB.classA", "Any"]],
+    [
+        [".classA", ["Any"]],
+        ["CLASss.classB.asdfas", ["Dict"]],
+        [".classB.classA", ["Any", "Dict"]],
+        ["classA", []],
+    ],
 )
 def test__get_values_from_matching_patterns__sanity(text, expected):
     # Arrange
     patterns = {
-        re.compile(r"\.*\.classA"): "Any",
-        re.compile(r"\.*\.classB\.*"): "Dict",
+        re.compile(r".*\.classA"): "Any",
+        re.compile(r".*\.classB\.*"): "Dict",
     }
-    expected = ["Any"]
 
     # Act + Assert
     assert get_values_from_matching_patterns(patterns, text) == expected
