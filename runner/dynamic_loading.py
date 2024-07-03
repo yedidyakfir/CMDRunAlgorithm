706813624f632fbbf815e52320b06c2050b117a4
yedid <yedidyakfir@gmail.com>
2024-07-03 14:26:25 +0300

    some object doesn't support issubclass function, ignore them

--- runner/dynamic_loading.py
+++ runner/dynamic_loading.py
@@ -16,20 +16,22 @@ def find_subclasses(module: ModuleType, base_class: type) -> List[type]:
         if is_pkg:
             subclasses.extend(find_subclasses(sub_module, base_class))
         else:
-            # Iterate over all objects in the module
             for obj_name, obj in inspect.getmembers(sub_module):
-                # Check if the object is a class, is a subclass of the base class,
-                # and is not the base class itself
-                if (
-                    inspect.isclass(obj)
-                    and issubclass(obj, base_class)
-                    and not inspect.isabstract(obj)
-                ):
-                    subclasses.append(obj)
+                try:
+                    if (
+                        inspect.isclass(obj)
+                        and issubclass(obj, base_class)
+                        and not inspect.isabstract(obj)
+                    ):
+                        subclasses.append(obj)
+                except TypeError:
+                    continue
     return list(set(subclasses))
 
 
-def find_class_by_name(module: ModuleType, class_name: str, only_class: bool = True) -> Optional[type]:
+def find_class_by_name(
+    module: ModuleType, class_name: str, only_class: bool = True
+) -> Optional[type]:
     for loader, name, is_pkg in pkgutil.walk_packages(module.__path__):
         full_name = module.__name__ + "." + name
         sub_module = importlib.import_module(full_name)
@@ -38,10 +40,13 @@ def find_class_by_name(module: ModuleType, class_name: str, only_class: bool = T
                 return klass
         else:
             for obj_name, obj in inspect.getmembers(sub_module):
-                if (
-                    (inspect.isclass(obj) or not only_class)
-                    and not inspect.isabstract(obj)
-                    and class_name == obj_name
-                ):
-                    return obj
+                try:
+                    if (
+                        (inspect.isclass(obj) or not only_class)
+                        and not inspect.isabstract(obj)
+                        and class_name == obj_name
+                    ):
+                        return obj
+                except TypeError:
+                    continue
     return None
