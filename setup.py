18bf3cd0db4821a893f195651404b02eddbd473a
yedid <yedidyakfir@gmail.com>
2024-07-18 20:56:22 +0300

    setup now ignores the test

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="cli-class-runner",
     version="0.0.1",
     author="Yedidya Kfir",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test", "test.*"]),
     install_requires=requirements,
     entry_points={"console_scripts": ["run_cli = my_package.__main__:main"]},
     description="This package will allow you to run any function and class of your code from the cli. "
