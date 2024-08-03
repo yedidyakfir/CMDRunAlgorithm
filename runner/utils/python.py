49e3abe627621d23ade1671a524845717de962fa
yedid <yedidyakfir@gmail.com>
2024-08-03 21:20:35 +0300

    added function to locate inner locaiton in dict

--- runner/utils/python.py
+++ runner/utils/python.py
@@ -30,3 +30,14 @@ PRIMITIVES = (
 
 def notation_belong_to_typing(annotation):
     return hasattr(annotation, "__module__") and annotation.__module__ == "typing"
+
+
+def location_in_dict(data: dict, location: str):
+    nested_location = location.split(".")
+    for i in range(len(nested_location), 0, -1):
+        inner_location = ".".join(nested_location[:i])
+        if isinstance(data, dict) and inner_location in data:
+            data = data[inner_location]
+        else:
+            return None
+    return data
